8c9929227e181c3da016f205095b1801f8fff3f4
peknight <JKpeknight@gmail.com>
2026-05-20 10:52:02 +0800

    chore: bump docker-build image versions
    - nginx: 1.30.0-alpine3.23 → 1.31.0-alpine3.23
    - postgres: 18.3-alpine3.23 → 18.4-alpine3.23
    - psql: 18.3 → 18.4
    - jenkins: 2.564-jdk25 → 2.565-jdk25
    - ollama: 0.23.2 → 0.24.0
    - openclaw: 2026.5.7 → 2026.5.18
    - nexus3: 3.92.0-alpine → 3.92.2-alpine

--- docker-build/shared/src/main/scala/com/peknight/docker/build/package.scala
+++ docker-build/shared/src/main/scala/com/peknight/docker/build/package.scala
@@ -15,9 +15,9 @@ package object build:
     /** @versionCheck https://hub.docker.com/v2/repositories/library/mysql/tags */
     val mysql: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "mysql"), Some(Tag("9.7.0")))
     /** @versionCheck https://hub.docker.com/v2/repositories/library/nginx/tags */
-    val nginx: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "nginx"), Some(Tag("1.30.0-alpine3.23")))
+    val nginx: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "nginx"), Some(Tag("1.31.0-alpine3.23")))
     /** @versionCheck https://hub.docker.com/v2/repositories/library/postgres/tags */
-    val postgres: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "postgres"), Some(Tag("18.3-alpine3.23")))
+    val postgres: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "postgres"), Some(Tag("18.4-alpine3.23")))
     /** @versionCheck https://hub.docker.com/v2/repositories/library/redis/tags */
     val redis: ImageRepositoryTag = ImageRepositoryTag(Repository(None, None, "redis"), Some(Tag("8.6.3-alpine3.23")))
     /** @versionCheck https://hub.docker.com/v2/repositories/library/ubuntu/tags */
@@ -25,7 +25,7 @@ package object build:
   end library
   object alpine:
     /** @versionCheck https://hub.docker.com/v2/repositories/alpine/psql/tags */
-    val psql: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("alpine")), "psql"), Some(Tag("18.3")))
+    val psql: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("alpine")), "psql"), Some(Tag("18.4")))
   end alpine
   object apolloconfig:
     val namespace: Namespace = Namespace("apolloconfig")
@@ -41,15 +41,15 @@ package object build:
   end gitea
   object jenkins:
     /** @versionCheck https://hub.docker.com/v2/repositories/jenkins/jenkins/tags */
-    val jenkins: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("jenkins")), "jenkins"), Some(Tag("2.564-jdk25")))
+    val jenkins: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("jenkins")), "jenkins"), Some(Tag("2.565-jdk25")))
   end jenkins
   object ollama:
     /** @versionCheck https://hub.docker.com/v2/repositories/ollama/ollama/tags */
-    val ollama: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("ollama")), "ollama"), Some(Tag("0.23.2")))
+    val ollama: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("ollama")), "ollama"), Some(Tag("0.24.0")))
   end ollama
   object openclaw:
     /** @versionCheck https://api.github.com/orgs/openclaw/packages/container/openclaw/versions */
-    val openclaw: ImageRepositoryTag = ImageRepositoryTag(Repository(Some(HostPort(host"ghcr.io")), Some(Namespace("openclaw")), "openclaw"), Some(Tag("2026.5.7")))
+    val openclaw: ImageRepositoryTag = ImageRepositoryTag(Repository(Some(HostPort(host"ghcr.io")), Some(Namespace("openclaw")), "openclaw"), Some(Tag("2026.5.18")))
   end openclaw
   object paulgauthier:
     /** @versionCheck https://hub.docker.com/v2/repositories/paulgauthier/aider/tags */
@@ -57,7 +57,7 @@ package object build:
   end paulgauthier
   object sonatype:
     /** @versionCheck https://hub.docker.com/v2/repositories/sonatype/nexus3/tags */
-    val nexus3: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("sonatype")), "nexus3"), Some(Tag("3.92.0-alpine")))
+    val nexus3: ImageRepositoryTag = ImageRepositoryTag(Repository(None, Some(Namespace("sonatype")), "nexus3"), Some(Tag("3.92.2-alpine")))
   end sonatype
   object rustdesk:
     /** @versionCheck https://hub.docker.com/v2/repositories/rustdesk/rustdesk-server/tags */
